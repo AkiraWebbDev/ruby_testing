a64e9c4efdd15918dab2075c6e4fd94462681806
Luke Webb <lukewebb2000@gmail.com>
2024-10-31 16:10:56 +1300

    Complete exercise 11

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -35,17 +35,20 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
-
+  subject(:tiggy) { described_class.new('Tiggy', 'Tuxedo', 'Black and white') }
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'when Cat reponds to shared method name talk' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(tiggy).not_to be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(tiggy).to be_hiding
   end
 end
